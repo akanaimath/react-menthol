8da4f31165a39ddc0da194f3f7c046722a6f92b2
akanaimath <39970348+akanaimath@users.noreply.github.com>
2019-04-01 10:48:38 -0500

    rolling in changes

--- push.sh
+++ push.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-#Will uncomment below line once all the components are working correctly
-#git add --all;git commit -m 'rolling in changes';npm version patch;npm publish;git push
-git push
+git add --all;git commit -m 'rolling in changes';npm version patch;npm publish;git push;
